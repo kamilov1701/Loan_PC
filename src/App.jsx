bec92590e441fdbd399e85d8905a53c2045f9102
kamilov1701 <azizbbekkamilov1701@gmail.com>
2026-03-27 17:58:26 +0500

    Start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='text-[45px] text-[red]'>Salom</h1>
+    </div>
+  )
+}
+
+export default App
